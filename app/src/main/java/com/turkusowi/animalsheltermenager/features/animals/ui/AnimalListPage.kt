e582dacd48c372f394acbe55ccba5e76d2e485b7
Kacper Dusza <kd131427@stud.ur.edu.pl>
2026-03-30 21:58:06 +0200

    Fix: fixed visual bugs (white letters on white background, fixed dog icon on AnimalPanelPage) added AnimalPanelPage to routes,

--- app/src/main/java/com/turkusowi/animalsheltermenager/features/animals/ui/AnimalListPage.kt
+++ app/src/main/java/com/turkusowi/animalsheltermenager/features/animals/ui/AnimalListPage.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.ui.tooling.preview.Preview
 import com.turkusowi.animalsheltermenager.features.animals.Animal
